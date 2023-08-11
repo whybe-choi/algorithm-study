5997a8ceb513b1680bda35199318161ce8fd5596
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2023-08-12 00:47:00 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,12 +1,3 @@
 def solution(s):
-    dic = {}
-    for i in s:
-        if i not in dic.keys():
-            dic[i] = 1
-        else:
-            dic[i] += 1
-    answer = []
-    for k in dic.keys():
-        if dic[k] == 1:
-            answer.append(k)
-    return "".join(sorted(answer))
+    answer = "".join(sorted([i for i in s if s.count(i) == 1]))
+    return answer
