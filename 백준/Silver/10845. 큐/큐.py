1c761c48721ccb77d334255012d3829df41d1f3c
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-01-25 19:08:34 +0900

    [Silver IV] Title: 큐, Time: 68 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline().rstrip())
+queue = deque()
+
+for _ in range(n):
+    prompt = sys.stdin.readline().rstrip()
+    if prompt == 'back':
+        print(queue[-1] if len(queue) != 0 else -1)
+    elif prompt == 'front':
+        print(queue[0] if len(queue) != 0 else -1)
+    elif prompt == 'empty':
+        print(1 if len(queue) == 0 else 0)
+    elif prompt == 'size':
+        print(len(queue))
+    elif prompt == 'pop':
+        print(queue.popleft() if len(queue) != 0 else -1)
+    else:
+        x = int(prompt.split()[-1])
+        queue.append(x)
+        
