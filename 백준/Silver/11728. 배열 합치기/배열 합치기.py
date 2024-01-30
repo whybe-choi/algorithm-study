a428dffc0f1422d28c2334d7fe7ee81d7da78e0a
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-01-30 14:32:10 +0900

    [Silver V] Title: 배열 합치기, Time: 1132 ms, Memory: 315920 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,9 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().rstrip().split())
+a = list(map(int, sys.stdin.readline().rstrip().split()))
+b = list(map(int, sys.stdin.readline().rstrip().split()))
+
+a.extend(b)
+
+print(" ".join(map(str, sorted(a))))
