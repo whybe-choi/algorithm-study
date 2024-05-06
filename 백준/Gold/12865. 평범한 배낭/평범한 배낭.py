6167f82cea892e6249097f034bbaac773fa02e88
Yong bin, Choi <64704608+whybe-choi@users.noreply.github.com>
2024-05-06 20:40:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 7188 ms, Memory: 282332 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,20 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().rstrip().split())
+
+bag = []
+for _ in range(n):
+    w, v = map(int, sys.stdin.readline().rstrip().split())
+    bag.append([w, v])
+
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+        if j >= bag[i-1][0]:
+            dp[i][j] = max(dp[i-1][j-bag[i-1][0]]+bag[i-1][1], dp[i-1][j])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+# 배낭에 넣을 수 있는 물건들의 가치합의 최댓값
+print(dp[n][k])
